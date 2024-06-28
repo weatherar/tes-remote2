2ff61bfbd8252222368753f132d3039b748d1a41
Aira <weatherar124@gmail.com>
2024-06-28 19:49:54 +0900

    mengedit file index.php

--- index.php
+++ index.php
@@ -8,6 +8,6 @@
 </head>
 <body>
     <h1>Hello world!</h1>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores perferendis dolorem vel voluptas tempore repudiandae facilis hic, obcaecati et cumque asperiores explicabo repellendus quod, adipisci eos natus debitis fugit eveniet!</p>
+    <p>Lorem dolor sit amet consectetur, adipisicing elit. Dolores perferendis dolorem vel voluptas tempore repudiandae facilis hic, obcaecati et cumque asperiores explicabo repellendus quod, adipisci eos natus debitis fugit eveniet!</p>
 </body>
 </html>
